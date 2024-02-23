4e81e4e5b0dfe0e8db12f1fb84484d2bd9429526
sajib689 <jibonraja11@gmail.com>
2024-02-23 17:56:04 +0600

    add api category

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+fetch('https://openapi.programming-hero.com/api/videos/categories')
   .then(res => res.json())
-  .then(data => myFunction(data.data));
+  .then(data => categories(data.data));
 
-const myFunction = (data) => {
-    const filterData = data.filter(singleData => singleData.category_id === '1001')
-    console.log(filterData)
-    filterData.map(singleData => {
-    const container = document.getElementById('container');
-    const createData = document.createElement('div'); // Fix the typo here
-   
-    createData.innerHTML = `
-      <div class="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src="${singleData.thumbnail}" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">${singleData.title}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-    `;
-
-    container.appendChild(createData);
-  });
-};
+const categories = (categories) => {
+    
+}
