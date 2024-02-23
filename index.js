5724b3908d74f86fc79de31b61949568814ff328
sajib689 <jibonraja11@gmail.com>
2024-02-23 18:11:32 +0600

    category added done

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ fetch('https://openapi.programming-hero.com/api/videos/categories')
   .then(data => categories(data.data));
 
 const categories = (categories) => {
+    const categoriesButton = document.getElementById('button-group')
     
+    categories.map(category => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button class="btn btn-active btn-ghost">${category.category}</button>
+
+        `
+        categoriesButton.appendChild(div)
+    })
+
 }
